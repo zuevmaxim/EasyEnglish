f6efcb75b2a15e099910bd49ce69c0e93817cecc
Maria Tigina <tiginamaria@gmail.com>
2019-04-26 15:52:57 +0300

    added  AddNewListActivity

---
+++ app/src/main/java/ru/hse/android/easyenglish/AddNewListActivity.java
@@ -0,0 +1,13 @@
+package ru.hse.android.easyenglish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_list);
+    }
+}
