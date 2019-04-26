6dd1a0c9b5ab86e21e4744213216868edb27efcd
Maria Tigina <tiginamaria@gmail.com>
2019-04-27 01:07:26 +0300

    added implementation for AddNewListActivity

--- app/src/main/java/ru/hse/android/easyenglish/AddNewListActivity.java
+++ app/src/main/java/ru/hse/android/easyenglish/AddNewListActivity.java
@@ -1,7 +1,14 @@
 package ru.hse.android.easyenglish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddNewListActivity extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class AddNewListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_list);
+
+        ListView newWordsListView = findViewById(R.id.new_word_lists);
+        final EditText russianWordText = findViewById(R.id.new_russian_word_text);
+        final EditText englishWordText = findViewById(R.id.new_english_word_text);
+
+        final ArrayList<String> newList = new ArrayList<>();
+        final ArrayList<Word> newWordsList = new ArrayList<>();
+        final ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, newList);
+
+        newWordsListView.setAdapter(adapter);
+
+        Button addWord = findViewById(R.id.add_word_button);
+        addWord.setOnClickListener(v -> {
+            String russianWord = russianWordText.getText().toString();
+            String englishWord = englishWordText.getText().toString();
+            if (russianWord.isEmpty()) {
+                russianWord = TranslateController.translate(englishWord, "en-ru");
+            }
+            if (englishWord.isEmpty()) {
+                englishWord = TranslateController.translate(russianWord, "ru-en");
+            }
+            newList.add(0, russianWord + " - " + englishWord);
+            newWordsList.add(new Word(russianWord, englishWord));
+            adapter.notifyDataSetChanged();
+            russianWordText.setText("");
+            englishWordText.setText("");
+        });
+
+        final EditText newWordLIstNameText = findViewById(R.id.new_list_name_text);
+        Button saveWordList = findViewById(R.id.save_list_button);
+        saveWordList.setOnClickListener(v -> {
+            String newWordLIstName = newWordLIstNameText.getText().toString();
+            WordListController controller = MainController.getGameController().getWordListController();
+            controller.addNewWordList(newWordLIstName, newWordsList);
+            Intent intent = new Intent( AddNewListActivity.this, WordListActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/ru/hse/android/easyenglish/WordListEditorActivity.java
+++ app/src/main/java/ru/hse/android/easyenglish/WordListEditorActivity.java
@@ -26,11 +26,13 @@ public class WordListEditorActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new WordListAdapter(this,  R.layout.list_item, wordListNames);
         wordLists.setAdapter(adapter);
         final Context context = this;
-        wordLists.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(context, "checked", Toast.LENGTH_LONG).show());
+        wordLists.setOnItemClickListener((parent, view, position, id) -> {
+                Toast.makeText(context, "checked", Toast.LENGTH_LONG).show();
+                });
 
         Button addNewListButton = findViewById(R.id.add_new_list_button);
         addNewListButton.setOnClickListener(v -> {
-            Intent intent = new Intent(WordListEditorActivity.this, ErrorActivity.class);
+            Intent intent = new Intent(WordListEditorActivity.this, AddNewListActivity.class);
             startActivity(intent);
         });
 
