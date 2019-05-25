321f728d1f4e609d4a3661aa625c25b17eae377d
Maxim Zuev <max-zu99@mail.ru>
2019-05-26 00:35:29 +0300

    fix word puzzle

--- app/src/main/java/ru/hse/android/project/easyenglish/WordPuzzleActivity.java
+++ app/src/main/java/ru/hse/android/project/easyenglish/WordPuzzleActivity.java
@@ -1,10 +1,8 @@
 package ru.hse.android.project.easyenglish;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 
 import ru.hse.android.project.easyenglish.adapters.DragAndDropAdapter;
-import ru.hse.android.project.easyenglish.controllers.GameController;
 import ru.hse.android.project.easyenglish.controllers.MainController;
 import ru.hse.android.project.easyenglish.controllers.Phrase;
 import ru.hse.android.project.easyenglish.controllers.PhraseStorage;
@@ -23,9 +20,9 @@ public class WordPuzzleActivity extends AppCompatActivity {
 
     private List<String> shuffleWords(Phrase phrase) {
         String englishPhrase = phrase.getEnglish();
-        List<String> words = Arrays.asList(englishPhrase.split(""));
-        List<String> shuffledWordResult = words;
-        while (shuffledWordResult.equals(words)) {
+        List<String> words = Arrays.asList(englishPhrase.split(" "));
+        List<String> shuffledWordResult = new ArrayList<>(words);
+        while (words.equals(shuffledWordResult)) {
             Collections.shuffle(shuffledWordResult);
         }
         return shuffledWordResult;
@@ -35,17 +32,12 @@ public class WordPuzzleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_puzzle);
-        /*
+
         PhraseStorage storage = MainController.getGameController().getPhraseStorage();
         Phrase phrase = storage.nextPhrase();
         List<String> words = shuffleWords(phrase);
         for (String word : words) {
             System.out.println(word);
-        }
-        */
-        List<String> words = new ArrayList<String>();
-        for(int i=1; i<7; i++){
-            words.add("Item " + i);
         }
 
         DragAndDropListView dragListView = findViewById(R.id.drag_and_drop_list);
