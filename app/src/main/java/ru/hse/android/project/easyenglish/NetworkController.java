828d538762df3e3e3f59dad178333731fed2467c
Maxim Zuev <max-zu99@mail.ru>
2019-05-29 21:43:28 +0300

    fix word chain history

--- app/src/main/java/ru/hse/android/project/easyenglish/NetworkController.java
+++ app/src/main/java/ru/hse/android/project/easyenglish/NetworkController.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -155,6 +156,7 @@ public class NetworkController extends AppCompatActivity {
             View dialogView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
             adb.setView(dialogView);
             RecyclerView recyclerView = dialogView.findViewById(R.id.list);
+            recyclerView.setLayoutManager(new LinearLayoutManager(NetworkController.this));
             List<Pair<String, String>> history = new ArrayList<>();
             for (Iterator<String> it = wordChain.getPreviousWords().iterator(); it.hasNext();) {
                 String word1 = it.next();
