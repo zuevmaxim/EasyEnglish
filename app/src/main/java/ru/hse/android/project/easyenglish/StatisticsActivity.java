4a403438fd06cbf7caa5ba6ff4b20b0a82af5fe6
Maria Tigina <tiginamaria@gmail.com>
2019-06-06 19:52:03 +0300

    added list names to learn words and statistics

--- app/src/main/java/ru/hse/android/project/easyenglish/StatisticsActivity.java
+++ app/src/main/java/ru/hse/android/project/easyenglish/StatisticsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +25,10 @@ public class StatisticsActivity extends AppCompatActivity {
         WordListController wordListController = MainController.getGameController().getWordListController();
         List<Word> words = wordListController.getCurrentListWords();
 
-        RecyclerView listView = findViewById(R.id.word_list_statistics);
+        TextView listNameText = findViewById(R.id.list_name_text);
+        listNameText.setText(wordListController.getCurrentWordList().toUpperCase());
 
+        RecyclerView listView = findViewById(R.id.word_list_statistics);
         StatisticsAdapter adapter = new StatisticsAdapter(this, words);
         listView.setAdapter(adapter);
 
