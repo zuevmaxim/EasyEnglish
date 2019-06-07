74da7ca3b5f3214731e8a3e549d798c444396ab7
Zuev Maxim <max-zu99@mail.ru>
2019-06-07 17:22:43 +0300

    add exception comments

--- app/src/main/java/ru/hse/android/project/easyenglish/exceptions/WrongListNameException.java
+++ app/src/main/java/ru/hse/android/project/easyenglish/exceptions/WrongListNameException.java
@@ -1,5 +1,8 @@
 package ru.hse.android.project.easyenglish.exceptions;
 
+/**
+ * Exception is thrown when list name is illegal in spelling or such list already exists.
+ */
 public class WrongListNameException extends Exception {
     public WrongListNameException(String message) {
         super(message);
